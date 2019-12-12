8be1e7a16b4934a7a352a21983361b3c3682773a
Tim <me@justim.net>
2019-12-12 18:13:44 +0100

    Add test for blackhole profiler

--- tests/ProfilerTest.php
+++ tests/ProfilerTest.php
@@ -13,7 +13,12 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Access\Database;
+use Access\Profiler\BlackholeProfiler;
+use Access\Query\Raw;
+
 use Tests\AbstractBaseTestCase;
+use Tests\Fixtures\Entity\User;
 
 class ProfilerTest extends AbstractBaseTestCase
 {
@@ -30,5 +35,35 @@ class ProfilerTest extends AbstractBaseTestCase
 
         // create table and inserts
         $this->assertEquals(7, count($export['queries']));
+    }
+
+    public function testBlackholeProfiler(): void
+    {
+        // create connection with blackhole profiler
+        $db = Database::create('sqlite::memory:', new BlackholeProfiler());
+
+        // do a bunch of queries on this connection..
+
+        $createUsersQuery = new Raw('CREATE TABLE `users` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+            `role` VARCHAR(20) DEFAULT NULL,
+            `name` VARCHAR(50) DEFAULT NULL,
+            `email` VARCHAR(150) DEFAULT NULL,
+            `created_at` DATETIME,
+            `updated_at` DATETIME
+        )');
+        $db->query($createUsersQuery);
+
+        $users = $db->findAll(User::class);
+        $this->assertEquals(0, count(iterator_to_array($users)));
+
+        $profiler = $db->getProfiler();
+        $export = $profiler->export();
+
+        // .. but nothing is tracked, preserving precious memory
+
+        $this->assertEquals(0.0, $export['duration']);
+        $this->assertEquals(0.0, $profiler->getTotalDuration());
+        $this->assertEquals(0, count($export['queries']));
     }
 }
